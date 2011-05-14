c3eebf7bca86ed5d75ff59347bda3bf09935467a
Yovoslav Ivanov <Yovoslav.Ivanov@gmail.com>
2011-05-14 18:00:25 +0200

    General include file for projects, which use EngineSupport. File must be included in the <projectname>-prefix.pch file.

---
+++ EngineSupport/Sources/Utilities/EngineSupport.h
@@ -0,0 +1,11 @@
+//
+//  EngineSupport.h
+//  EngineSupport
+//
+//  Created by Yovoslav Ivanov on 5/14/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "NSDictionary+Parameters.h"
+#import "Utilities.h"
+#import "Parameters.h"
